0798e49012299f815a7374f097e6dc92ec50b7b9
Ritesh Ghanta <ritesh.0927@outlook.com>
2020-07-19 22:38:26 +0530

    try eigen, some cleanup

--- ren_e.cpp
+++ ren_e.cpp
@@ -23,14 +23,11 @@
 
 #include <SFML/Graphics.hpp>
 
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/Surface_mesh.h>
-#include <CGAL/convex_hull_3.h>
-#include <vector>
+#include <Eigen/Dense>
+#include <Eigen/LU>
 
-#include <CGAL/Vector_2.h>
-#include <CGAL/Vector_3.h>
-#include <CGAL/Aff_transformation_3.h>
+#include <iostream>
+#include <vector>
 
 #define CLIP_W 100
 #define CLIP_W 100
@@ -38,48 +35,46 @@
 #define IMG_H 300
 #define DISTS -5
 
-typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-typedef K::Point_3 Point_3;
-typedef K::Point_2 Point_2;
-typedef CGAL::Vector_2<K> Vector_2;
-typedef CGAL::Vector_3<K> Vector_3;
-typedef CGAL::Surface_mesh<Point_3> Surface_mesh;
+typedef Eigen::Matrix3d Tr;
+typedef Eigen::Vector2d Vector_2;
+typedef Eigen::Vector3d Vector_3;
 
-typedef CGAL::Aff_transformation_3<K> Transformation;
+Vector_3 translate(0, 0, -30);
 
-Transformation translate(CGAL::TRANSLATION, Vector_3(0, 0, -30));
-Transformation scale(CGAL::SCALING, 100);
+Tr scale = (Tr() <<
+	100,   0,   0,
+	  0, 100,   0,
+    0,   0, 100
+).finished();
 
-Transformation rotate(
-		0.866,   0.5, 0,
-		 -0.5, 0.866, 0,
-		    0,     0, 1,
-		    1
-);
+Tr rotate = (Tr() <<
+	0.866,   0.5, 0,
+	 -0.5, 0.866, 0,
+	    0,     0, 1
+).finished();
 
 // invert y
-Transformation invert_y(
+Tr invert_y = (Tr() <<
 		1,  0, 0,
 		0, -1, 0,
-		0,  0, 1,
-    1
-);
+		0,  0, 1
+).finished();
 
-Point_3 cube[8] = {
-	Point_3(0, 0, 0),
-	Point_3(1, 0, 0),
-	Point_3(1, 1, 0),
-	Point_3(0, 1, 0),
-	Point_3(0, 0, 1),
-	Point_3(1, 0, 1),
-	Point_3(1, 1, 1),
-	Point_3(0, 1, 1)
+Vector_3 cube[8] = {
+	Vector_3(0, 0, 0),
+	Vector_3(1, 0, 0),
+	Vector_3(1, 1, 0),
+	Vector_3(0, 1, 0),
+	Vector_3(0, 0, 1),
+	Vector_3(1, 0, 1),
+	Vector_3(1, 1, 1),
+	Vector_3(0, 1, 1)
 };
 
-Point_3 triangle[3] = {
-	Point_3(0, 0, -0.03),
-	Point_3(1, 0, 0.01),
-	Point_3(0, 1, 0)
+Vector_3 triangle[3] = {
+	Vector_3(0, 0, -0.03),
+	Vector_3(1, 0, 0.01),
+	Vector_3(0, 1, 0)
 };
 
 // 222,  69,  72
@@ -89,23 +84,22 @@ Point_3 triangle[3] = {
 #define NUM_T 1
 
 /*
-Point_3 triang[NUM_T * 3][2] = {
-	{ Point_3(0, 0, -60), Point_3(222,  69,  72) },
-	{ Point_3(100, 0,  0), Point_3(222,  69,  72) },
-	{ Point_3(0, 100,  0), Point_3(222,  69,  72) },
+Vector_3 triang[NUM_T * 3][2] = {
+	{ Vector_3(0, 0, -60), Vector_3(222,  69,  72) },
+	{ Vector_3(100, 0,  0), Vector_3(222,  69,  72) },
+	{ Vector_3(0, 100,  0), Vector_3(222,  69,  72) },
 
-	{ Point_3(0, 0, -30), Point_3( 38, 128, 167) },
-	{ Point_3(100, 0, -30), Point_3( 38, 128, 167) },
-	{ Point_3(0, 100, -30), Point_3( 38, 128, 167) }
+	{ Vector_3(0, 0, -30), Vector_3( 38, 128, 167) },
+	{ Vector_3(100, 0, -30), Vector_3( 38, 128, 167) },
+	{ Vector_3(0, 100, -30), Vector_3( 38, 128, 167) }
 };
 */
 
-Point_3 triang[NUM_T * 3][2] = {
-	{ Point_3(0, 0, 0), Point_3(222,  69,  72) },
-	{ Point_3(200, 0,  0), Point_3( 22, 124,  76) },
-	{ Point_3(0, 200,  0), Point_3(38, 128, 167) }
+Vector_3 triang[NUM_T * 3][2] = {
+	{ Vector_3(0, 0, 0), Vector_3(222,  69,  72) },
+	{ Vector_3(200, 0,  0), Vector_3( 22, 124,  76) },
+	{ Vector_3(0, 200,  0), Vector_3(38, 128, 167) }
 };
-
 
 sf::Uint8* pixels = new sf::Uint8[IMG_W * IMG_H * 4];
 double zarray[IMG_W][IMG_H] = {0};
@@ -124,18 +118,15 @@ int main(int argc, char* argv[])
 {
   sf::RenderWindow window(sf::VideoMode(IMG_W, IMG_H), "triangles", sf::Style::Titlebar | sf::Style::Close);
 
-  Surface_mesh sm;
-  CGAL::convex_hull_3(triangle, triangle + 3, sm);
-
   int n_faces = 0;
 
   // Step 1: Project Primitives
   int v_index = 0;
-  Point_2 v_t_p[3];
-  Point_3 tp;
+  Vector_2 v_t_p[3];
+  Vector_3 tp;
   double z_i_t[3];
 
-  Point_2 t_pixel;
+  Vector_2 t_pixel;
 
   float ratio = (IMG_W / CLIP_W);
 
@@ -150,12 +141,12 @@ int main(int argc, char* argv[])
       // transform the object in any way
       // apply the T to tp
       //tp = scale(tp);
-      tp = rotate(tp);
-      tp = invert_y(tp);
-      tp = translate(tp);
+      tp = invert_y * tp;
+      tp = rotate * tp;
+      tp = translate + tp;
 
       // project
-      v_t_p[v_index] = Point_2(DISTS * tp.x() / tp.z(), DISTS * tp.y() / tp.z());
+      v_t_p[v_index] = Vector_2(DISTS * tp.x() / tp.z(), DISTS * tp.y() / tp.z());
 
       // store inverted z for later computation
       z_i_t[v_index] = (1.0 / tp.z());
@@ -168,17 +159,17 @@ int main(int argc, char* argv[])
     Vector_3 b_1 = Vector_3(v_t_p[1].x(), v_t_p[1].y(), 1);
     Vector_3 c_1 = Vector_3(v_t_p[2].x(), v_t_p[2].y(), 1);
 
-    double det_t = CGAL::determinant(a_1, b_1, c_1);
+    double det_t = (Tr() << a_1, b_1, c_1).finished().determinant();
 
     for(int i = 0; i < (IMG_W - 1); i++) {
       for(int j = 0; j < (IMG_H - 1); j++) {
-        t_pixel = Point_2(((float)((float)i - (IMG_W / 2))) / ratio, ((float)((float)j - (IMG_H / 2))) / ratio);
+        t_pixel = Vector_2(((float)((float)i - (IMG_W / 2))) / ratio, ((float)((float)j - (IMG_H / 2))) / ratio);
 
         Vector_3 tp_1 = Vector_3(t_pixel.x(), t_pixel.y(), 1);
 
-        double alpha_d = CGAL::determinant(tp_1, b_1, c_1) / det_t;
-        double beta_d = CGAL::determinant(a_1, tp_1, c_1) / det_t;
-        double gamma_d = CGAL::determinant(a_1, b_1, tp_1) / det_t;
+        double alpha_d = (Tr() << tp_1, b_1, c_1).finished().determinant() / det_t;
+        double beta_d = (Tr() << a_1, tp_1, c_1).finished().determinant() / det_t;
+        double gamma_d = (Tr() << a_1, b_1, tp_1).finished().determinant() / det_t;
 
         if((alpha_d >= 0) && (beta_d >= 0) && (gamma_d >= 0)) {
           // pixel inside triangle
@@ -190,9 +181,9 @@ int main(int argc, char* argv[])
           depth = (-1.0) * depth; // now depth is positive
 
           if(depth > zarray[i][j]) {
-            Vector_3 v1c = Vector_3(Point_3(0, 0, 0), triang[(3 * p) + 0][1]);
-            Vector_3 v2c = Vector_3(Point_3(0, 0, 0), triang[(3 * p) + 1][1]);
-            Vector_3 v3c = Vector_3(Point_3(0, 0, 0), triang[(3 * p) + 2][1]);
+            Vector_3 v1c = triang[(3 * p) + 0][1];
+            Vector_3 v2c = triang[(3 * p) + 1][1];
+            Vector_3 v3c = triang[(3 * p) + 2][1];
 
             Vector_3 pix_c = (alpha_d * v1c * z_i_t[0]) +
                              (beta_d  * v2c * z_i_t[1]) +
